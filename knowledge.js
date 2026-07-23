563a371f757ae6cac79d098cca170d9df118c508
Gegablox-Ai <rupa067079@gmail.com>
2026-07-23 18:56:50 +0530

    Add knowledge object with game information

---
+++ knowledge.js
@@ -0,0 +1,15 @@
+const knowledge = {
+    "owner": "The owner of GigaBlocks.ai is Rudransh.",
+
+    "bloxd.io": "Bloxd.io is a multiplayer sandbox game where players can build, craft, explore, and play different game modes.",
+
+    "release": "Bloxd.io was released in 2021.",
+
+    "blocks": "Bloxd.io has many types of blocks used for building and creating structures.",
+
+    "commands": "Bloxd.io commands can be used to control different game features.",
+
+    "mobs": "Bloxd.io has different mobs and creatures in the game.",
+
+    "game modes": "Bloxd.io has multiple game modes, including survival and creative modes."
+};
